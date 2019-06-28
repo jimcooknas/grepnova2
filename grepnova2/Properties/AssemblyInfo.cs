9ea0daa2a0043d31272ab1fafc275fc55f5a0250
jimcooknas <cooknas@gmail.com>
2019-06-28 17:50:49 +0300

    Add files via upload

---
+++ grepnova2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("grepnova2")]
+[assembly: AssemblyDescription("A Utility for Amateur Supernova Patrolling created for Greek Supernovae Search Team")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Initial Concept and Code by Dominic Ford. ReCoded in C# by Cooknas for Greek Supernovae Search Team")]
+[assembly: AssemblyProduct("grepnova2")]
+[assembly: AssemblyCopyright("Licensed under GNU GPL")]//Copyright © Cooknas Bros 2019 under GNU GPL")]
+[assembly: AssemblyTrademark("Free for amatour hunters")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f729f82-abd8-4fab-adbe-1c282986422b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
